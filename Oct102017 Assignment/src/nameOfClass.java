f3483e3f0dd958065a87c9047fc1e2373de15c7f
SabienNguyen <32147674+SabienNguyen@users.noreply.github.com>
2017-10-03 17:47:37 -0700

    This is assignment #1 of robotics

---
+++ Oct102017 Assignment/src/nameOfClass.java
@@ -0,0 +1,13 @@
+//Sabien Nguyen
+//The below line shows what kind of code this is and the name of the code
+public class nameOfClass {
+//This is the main method, this is use to do the function after
+	public static void main (String[] args) {
+		//This prints the below line, the below line is a statement that prints to the console
+		System.out.print("The head flew off the body with the flick of the king's wrist.");
+		//This line uses println, println makes the statements appear on the same line
+		System.out.println(" John's body quivered as his decapitated head laid on the ground.");
+		//This line does NOT use priintln and just uses print which is why the statement appears on the second line
+		System.out.print("John's head was left there for it to become the foood of the wild animals, which inhabit the area.");
+	}
+}

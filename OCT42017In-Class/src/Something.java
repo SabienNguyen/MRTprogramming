596ccb5ed87b4319db018562697b0f6ff2e25745
mhs <mhs@MHS-Science2-13>
2017-10-04 16:48:41 -0700

    10/4/17

---
+++ OCT42017In-Class/src/Something.java
@@ -0,0 +1,19 @@
+
+public class Something {
+	public static void main(String[] args){
+		int number = 3;
+		System.out.println(number);
+		char letter = 'n';
+		System.out.println(letter);
+		double dec = 10.5;
+		System.out.println(dec);
+		boolean duck = true;
+		System.out.println(duck);
+		String duckAlpha = "jack climbed the tree";
+		System.out.println(duckAlpha);
+		
+		
+		
+	}
+	
+}

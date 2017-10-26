dfb3dbbf6b49fed1c60f1d9e8946315678dc2540
mhs <mhs@MHS-Science3-13>
2017-10-25 17:05:34 -0700

    Commit

---
+++ Some more java/src/MethodPractice.java
@@ -0,0 +1,14 @@
+
+public class MethodPractice{
+	public static void main(String[] args){
+		double a = 10.5;
+		double b = 5.5;
+	}
+	public double max(){	
+		if(a > b){
+			return a;
+		}else{
+			return b;
+		}
+	}
+}	

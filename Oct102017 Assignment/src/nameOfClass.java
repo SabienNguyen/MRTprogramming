8596e3cbfd3582e49851a8189e5e03885d5f26ef
SabienNguyen <32147674+SabienNguyen@users.noreply.github.com>
2017-11-28 23:02:53 -0800

    uhhhhhhhhhh

--- Oct102017 Assignment/src/nameOfClass.java
+++ Oct102017 Assignment/src/nameOfClass.java
@@ -7,7 +7,7 @@ public class nameOfClass {
 		System.out.print("The head flew off the body with the flick of the king's wrist.");
 		//This line uses println, println makes the statements appear on the same line
 		System.out.println(" John's body quivered as his decapitated head laid on the ground.");
-		//This line does NOT use priintln and just uses print which is why the statement appears on the second line
+		//This line does NOT use println and just uses print which is why the statement appears on the second line
 		System.out.print("John's head was left there for it to become the foood of the wild animals, which inhabit the area.");
 	}
 }
